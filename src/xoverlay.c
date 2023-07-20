b3ac5f73480355a1e58bf928f01e159c77149441
Runker45 <137824545+Runker45@users.noreply.github.com>
2023-07-20 10:17:29 -0700

    Update xoverlay.c

--- src/xoverlay.c
+++ src/xoverlay.c
@@ -4,27 +4,20 @@
  *  Created on: Nov 9, 2017
  *      Author: nullifiedcat
  */
-
 #include "internal/drawglx.h"
-
 #include <GL/glx.h>
 #include <X11/extensions/shape.h>
 #include <X11/extensions/Xfixes.h>
-
 #include <string.h>
 #include <stdio.h>
 #include <xoverlay.h>
-
 #include <dlfcn.h> // dlsym
 
 int event_ShapeNotify;
 int event_ShapeError;
 struct xoverlay_library xoverlay_library;
-
-typedef void ( *glXSwapBuffers_t) (Display *dpy, GLXDrawable drawable);
+typedef void (*glXSwapBuffers_t)(Display *dpy, GLXDrawable drawable);
 glXSwapBuffers_t glXSwapBuffersfn;
-
-int preinit_done = 0;
 
 int xoverlay_init()
 {
@@ -32,38 +25,26 @@ int xoverlay_init()
 
     xoverlay_library.display = XOpenDisplay(NULL);
     if (xoverlay_library.display == NULL)
-    {
         return -2;
-    }
 
     xoverlay_library.screen = DefaultScreen(xoverlay_library.display);
-    xoverlay_library.width =
-        DisplayWidth(xoverlay_library.display, xoverlay_library.screen);
-    xoverlay_library.height =
-        DisplayHeight(xoverlay_library.display, xoverlay_library.screen);
+    xoverlay_library.width = DisplayWidth(xoverlay_library.display, xoverlay_library.screen);
+    xoverlay_library.height = DisplayHeight(xoverlay_library.display, xoverlay_library.screen);
 
-    if (!XShapeQueryExtension(xoverlay_library.display, &event_ShapeNotify,
-                              &event_ShapeError))
-    {
+    if (!XShapeQueryExtension(xoverlay_library.display, &event_ShapeNotify, &event_ShapeError))
         return -3;
-    }
 
     if (xoverlay_glx_init() < 0)
-    {
         return -4;
-    }
-    if (xoverlay_glx_create_window() < 0)
-    {
-        return -5;
-    }
 
-    glXSwapBuffersfn = (glXSwapBuffers_t) dlsym((void *)0xFFFFFFFF, "glXSwapBuffers"); // copied from gameoverlayrenderer.so
-    
-    if (!glXSwapBuffersfn) // not my problem
-    {
+    if (xoverlay_glx_create_window() < 0)
+        return -5;
+
+    glXSwapBuffersfn = (glXSwapBuffers_t)dlsym((void *)0xFFFFFFFF, "glXSwapBuffers");
+
+    if (!glXSwapBuffersfn)
         return -6;
-    }
-    
+
     xoverlay_library.init = 1;
 
     return 0;
@@ -71,6 +52,9 @@ int xoverlay_init()
 
 void xoverlay_destroy()
 {
+    if (!xoverlay_library.init)
+        return;
+
     XDestroyWindow(xoverlay_library.display, xoverlay_library.window);
     XCloseDisplay(xoverlay_library.display);
     xoverlay_glx_destroy();
@@ -79,36 +63,34 @@ void xoverlay_destroy()
 
 void xoverlay_show()
 {
-    if (xoverlay_library.mapped == 1)
-        return;
-
-    XMapWindow(xoverlay_library.display, xoverlay_library.window);
-    xoverlay_library.mapped = 1;
+    if (xoverlay_library.window && !xoverlay_library.visible) {
+        XMapWindow(xoverlay_library.display, xoverlay_library.window);
+        xoverlay_library.visible = 1;
+    }
 }
 
 void xoverlay_hide()
 {
-    if (xoverlay_library.mapped == 0)
-        return;
-
-    XUnmapWindow(xoverlay_library.display, xoverlay_library.window);
-    xoverlay_library.mapped = 0;
+    if (xoverlay_library.window && xoverlay_library.visible) {
+        XUnmapWindow(xoverlay_library.display, xoverlay_library.window);
+        xoverlay_library.visible = 0;
+    }
 }
 
 void xoverlay_draw_begin()
 {
-    if (!xoverlay_library.init)
+    if (!xoverlay_library.init || xoverlay_library.drawing)
         return;
+
     xoverlay_library.drawing = 1;
-    glXMakeCurrent(xoverlay_library.display, xoverlay_library.window,
-                   glx_state.context);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glXMakeCurrent(xoverlay_library.display, xoverlay_library.window, glx_state.context);
 }
 
 void xoverlay_draw_end()
 {
-    if (!xoverlay_library.init)
+    if (!xoverlay_library.init || !xoverlay_library.drawing)
         return;
+
     glXSwapBuffersfn(xoverlay_library.display, xoverlay_library.window);
     xoverlay_library.drawing = 0;
 }
